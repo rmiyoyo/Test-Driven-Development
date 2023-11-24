22e434821385bfcf0531aa65f2f1afe14f42689b
Ismail mayito <ismayito1@gmail.com>
2023-11-24 16:38:30 +0300

    Add test for negative arguement error

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,6 +4,21 @@ describe Solver do
 
 
     describe '#factorial' do
+
+    it 'should return 1 if n is either zero(0) or one(1)' do
+        solver = Solver.new
+        factorial_number_one = solver.factorial(1)
+        factorial_number_zero = solver.factorial(0)
+        expect(factorial_number_one).to eq(1)
+        expect(factorial_number_zero).to eq(1)
+    end
+
+    it 'should raise an error n is a negative number' do 
+        solver = Solver.new
+        factorial_number_negative = solver.factorial(-1)
+        expect(factorial_number_negative).to raise_error(ArgumentError, "n must be a positive number")
+    end
+
     it 'should return factorial of number n' do
         solver = Solver.new
         factorial_number = solver.factorial(5)
