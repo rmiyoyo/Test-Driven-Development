e51501a2b8028e403d96c7bc47998d277ecba155
Ismail mayito <ismayito1@gmail.com>
2023-11-24 15:53:29 +0300

    Add test for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,3 +1,11 @@
+require_relative '../solver'
 
 describe Solver do 
+
+    describe '#factorial' do
+    it 'should return factorial of number N' do
+        factorial_number = factorial(5)
+        expect(factorial_number).to be(5*(5-1))
+    end
+end
 end
