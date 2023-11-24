31e573cacfcceaf8b0587701f71a32708a731966
Ismail mayito <ismayito1@gmail.com>
2023-11-24 17:09:41 +0300

    fix linter errors

--- solver.rb
+++ solver.rb
@@ -1,12 +1,11 @@
-class Solver 
-
-    def factorial(n) 
-        if n == 1 || n==0 
-         return 1
-        elsif n < 0
-         raise ArgumentError, "n must be a positive number" 
-        else
-          return  n*factorial(n-1)
-        end
+class Solver
+  def factorial(number)
+    if [1, 0].include?(number)
+      1
+    elsif n.negative?
+      raise ArgumentError, 'number must be a positive number'
+    else
+      number * factorial(number - 1)
     end
+  end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,27 +1,24 @@
 require_relative '../solver'
 
-describe Solver do 
-
-
-    describe '#factorial' do
-
+describe Solver do
+  describe '#factorial' do
     it 'should return 1 if n is either zero(0) or one(1)' do
-        solver = Solver.new
-        factorial_number_one = solver.factorial(1)
-        factorial_number_zero = solver.factorial(0)
-        expect(factorial_number_one).to eq(1)
-        expect(factorial_number_zero).to eq(1)
+      solver = Solver.new
+      factorial_number_one = solver.factorial(1)
+      factorial_number_zero = solver.factorial(0)
+      expect(factorial_number_one).to eq(1)
+      expect(factorial_number_zero).to eq(1)
     end
 
-    it 'should raise an error n is a negative number' do 
-        solver = Solver.new
-        expect{ solver.factorial(-1)}.to raise_error(ArgumentError, "n must be a positive number")
+    it 'should raise an error n is a negative number' do
+      solver = Solver.new
+      expect { solver.factorial(-1) }.to raise_error(ArgumentError, 'n must be a positive number')
     end
 
     it 'should return factorial of number n' do
-        solver = Solver.new
-        factorial_number = solver.factorial(5)
-        expect(factorial_number).to eql(120)
+      solver = Solver.new
+      factorial_number = solver.factorial(5)
+      expect(factorial_number).to eql(120)
     end
-end
+  end
 end
