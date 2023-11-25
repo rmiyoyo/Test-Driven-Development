1a3313122cc667696d5c023c680340043f21c33e
rmiyoyo <miyoyoraphael@gmail.com>
2023-11-25 11:11:42 +0300

    Add fizzbuzz test

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -29,4 +29,30 @@ describe Solver do
       expect(string_to_reverse).to eq('otiyam')
     end
   end
+
+  describe '#fizzbuzz' do
+    it 'should return "fizz" when N is divisible by 3' do
+      solver = Solver.new
+      result = solver.fizzbuzz(9)
+      expect(result).to eq('fizz')
+    end
+
+    it 'should return "buzz" when N is divisible by 5' do
+      solver = Solver.new
+      result = solver.fizzbuzz(10)
+      expect(result).to eq('buzz')
+    end
+
+    it 'should return "fizzbuzz" when N is divisible by both 3 and 5' do
+      solver = Solver.new
+      result = solver.fizzbuzz(15)
+      expect(result).to eq('fizzbuzz')
+    end
+
+    it 'should return N as a string when N is not divisible by 3 or 5' do
+      solver = Solver.new
+      result = solver.fizzbuzz(7)
+      expect(result).to eq('7')
+    end
+  end
 end
